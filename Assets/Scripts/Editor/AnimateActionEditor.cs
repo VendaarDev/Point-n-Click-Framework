b8cd39863878fb0e5d548738a2cadc8b064e1277
Vendaar <79939612+VendaarDev@users.noreply.github.com>
2021-09-16 17:26:52 +0200

    finished AnimateActionEditor
    Finished custom animation inspector so you can link actions to happen after a animation is played

--- Assets/Scripts/Editor/AnimateActionEditor.cs
+++ Assets/Scripts/Editor/AnimateActionEditor.cs
@@ -23,13 +23,33 @@ public class AnimateActionEditor : Editor
         }
 
         //draw anim inspector
+        for (int i = 0; i < anims.arraySize; i++)
+        {
+             DrawAnimsInspector(anims.GetArrayElementAtIndex(i), i);
+        }
 
+        EditorExtensions.DrawActionsArray(actions, "Actions Chaining:");
 
         serializedObject.ApplyModifiedProperties();
    }
 
    void DrawAnimsInspector(SerializedProperty entry, int id)
    {
+        GUILayout.BeginVertical("box");
 
+        GUILayout.BeginHorizontal();
+
+        EditorGUILayout.PropertyField(entry.FindPropertyRelative("triggerName"), new GUIContent("Trigger Name:"));
+
+        if (GUILayout.Button("X", GUILayout.Width(20f)))
+        {
+             anims.DeleteArrayElementAtIndex(id);
+             return;
+        }
+        GUILayout.EndHorizontal();
+
+        EditorGUILayout.PropertyField(entry.FindPropertyRelative("invokeDelay"), new GUIContent("Delay (in sec):"));
+
+        GUILayout.EndVertical();
    }
 }
